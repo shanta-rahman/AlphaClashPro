be95e5eebad3c2cc8ab8650debf7437958bc8cb0
shanta-rahman <shantarahman060@gmail.com>
2024-09-15 19:18:00 +0600

    updated score and life

--- alpha-clash.js
+++ alpha-clash.js
@@ -12,9 +12,63 @@
 // }
 
 
-function handleKeyboardButtonPress(){
-    console.log('button pressed');
+function handleKeyboardButtonPress(event){
+    const playerPressed = event.key;
+
+ console.log('player pressed' , playerPressed);
+
+//  get the expected to press
+
+ const currentAlphabateElement = document.getElementById('current-alphabate');
+
+const currentAlphabate = currentAlphabateElement.innerText;
+const expectedAlphabate = currentAlphabate.toLowerCase();
+console.log(playerPressed,expectedAlphabate);
+
+
+//Check matched or not 
+
+if(playerPressed==expectedAlphabate)
+{
+    console.log('You got a point');
+    
+// Update score
+   // 1. get the current score
+
+   const currentScoreElement= document.getElementById('current-score');
+   const currentScoreText = currentScoreElement.innerText;
+   const currentScore = parseInt(currentScoreText);
+   console.log(currentScore);
+   // 2. increase the score by 1
+const newScore = currentScore + 1;
+
+   // 3. show the updated score
+currentScoreElement.innerText = newScore;
+
+    // start a new round
+    removeBackgroundColorById(expectedAlphabate);
+    continueGame();
 }
+else
+{
+    console.log ('you missed ,you lost a lifes');
+
+//step 1 : get the current Life number
+const currentLifeElement = document.getElementById('current-life')
+const currentLifeText = currentLifeElement.innerText;
+const currentLife = parseInt(currentLifeText);
+
+//atep 2: reduce the life count
+const newLife = currentLife - 1 ;
+
+
+//step 3 : display the updated life count
+currentLifeElement.innerText=newLife;
+
+}
+}
+
+// Capyured keyboard key press
 
 document.addEventListener('keyup' , handleKeyboardButtonPress )
 
--- utility.js
+++ utility.js
@@ -15,6 +15,21 @@ function setBackgroundColorById(elementId)
     element.classList.add('bg-orange-500');
 }
 
+function removeBackgroundColorById(elementId)
+{
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-500');
+}
+
+
+function getTextElementValueById(elementId)
+{
+    const element = document.getElementById(elementId);
+    const elemenyValueText = element.innerText;
+    const value = parseInt(elemenyValueText);
+    return value
+}
+
 function getARandomAlphabate()
 {//get or create an alphabet array
     const alphabatesString = 'abhdeuudkoenfuyeqwjvugerjcbyrvf' ;
